ed5df0cae1199ec232b62f8bb6bbf0bf27946883
Edgarskonnegad <j.edgarskonnegard@gmail.com>
2024-10-29 15:10:55 +0100

    classes

--- Program.cs
+++ Program.cs
@@ -5,7 +5,51 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!");
+            var player = new Player();
             
         }
     }
+}
+
+class Player
+{
+    List<GameConsole> gameConsole = new List<GameConsole>();
+    public AddGame(GameConsole console)
+    {
+        gameConsole.Add(console);
+    }
+}
+
+class GameConsole
+{
+    GameLibrary gameLibrary = new GameLibrary();
+}
+
+class Game
+{
+    Level level = new Level();
+    Processor processor = new Processor();
+    GraphicsCard graphicsCard = new GraphicsCard();
+}
+class Level
+{
+
+}
+
+class GameLibrary
+{
+    List<Game> games = new List<Game>();
+
+    public AddGame(Game game)
+    {
+        gameLibrary.Add(game);
+    }
+}
+class Processor
+{
+
+}
+class GraphicsCard
+{
+
 }
