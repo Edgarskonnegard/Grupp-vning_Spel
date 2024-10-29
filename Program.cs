9f1a12dbc98bbcfd86d7fcef64601a91e08778cf
Edgarskonnegad <j.edgarskonnegard@gmail.com>
2024-10-29 15:25:45 +0100

    k

--- Program.cs
+++ Program.cs
@@ -6,50 +6,78 @@
         {
             Console.WriteLine("Hello World!!");
             var player = new Player();
-            
+            var gameConsole = new GameConsole("PS5");
+            player.AddGameConsole(gameConsole)
+            var game = new Game("SuperLuigiSisters")
+
         }
     }
 }
 
-class Player
+public class Player
 {
     List<GameConsole> gameConsole = new List<GameConsole>();
-    public AddGameConsole(GameConsole console)
+    public void AddGameConsole(GameConsole console)
     {
         gameConsole.Add(console);
     }
 }
 
-class GameConsole
+public class GameConsole
 {
-    GameLibrary gameLibrary = new GameLibrary();
-}
+    public string Name { get; set; }
 
-class Game
-{
-    Level level = new Level();
+    public GameConsole(string name)
+    {
+        Name = name;
+    }
+    GameLibrary gameLibrary = new GameLibrary();
+
     Processor processor = new Processor();
     GraphicsCard graphicsCard = new GraphicsCard();
+
+    public void ChooseGame()
+    {
+        gameLibrary.Show();
+    }
 }
-class Level
+
+public class Game
+{
+    public string Name { get; set; }
+    public Game(string name)
+    {
+        Name = name;
+    }
+    Level level = new Level();
+}
+public class Level
 {
 
 }
 
-class GameLibrary
+public class GameLibrary
 {
     List<Game> games = new List<Game>();
+
+    public void Show()
+    {
+        foreach (var game in games)
+        {
+            Console.WriteLine(game.Name);
+        }
+    }
 
     public AddGame(Game game)
     {
         gameLibrary.Add(game);
     }
 }
-class Processor
+public class Processor
 {
 
 }
-class GraphicsCard
+public class GraphicsCard
 {
 
 }
